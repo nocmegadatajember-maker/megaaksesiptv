06f165909ac57f720edc573bd41f528cb2689ea2
nocmegadatajember <nocmegadatajember@gmail.com>
2026-02-07 18:37:20 +0700

    Add HLS/M3U8 video player implementation using Video.js

--- js/player.js
+++ js/player.js
@@ -1,35 +1,28 @@
-class IPTVPlayer {
-    constructor(videoElement) {
-        this.videoElement = videoElement;
-        this.hls = null;
-    }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://vjs.zencdn.net/7.11.4/video-js.css" rel="stylesheet">
+    <title>HLS/M3U8 Video Player</title>
+</head>
+<body>
+    <video
+        id="my-video"
+        class="video-js"
+        controls
+        preload="auto"
+        width="640"
+        height="264"
+        data-setup="{}"
+    >
+        <source src="YOUR_M3U8_URL_HERE.m3u8" type="application/x-mpegURL">
+        <p class="vjs-no-js">
+            To view this video please enable JavaScript, and consider upgrading to a web browser that
+            <a href="https://instructure.com">supports HTML5 video</a>
+        </p>
+    </video>
 
-    loadStream(streamUrl) {
-        if (Hls.isSupported()) {
-            this.hls = new Hls();
-            this.hls.loadSource(streamUrl);
-            this.hls.attachMedia(this.videoElement);
-            this.hls.on(Hls.Events.MANIFEST_PARSED, () => {
-                this.videoElement.play();
-            });
-        } else if (this.videoElement.canPlayType('application/vnd.apple.mpegurl')) {
-            this.videoElement.src = streamUrl;
-            this.videoElement.addEventListener('loadedmetadata', () => {
-                this.videoElement.play();
-            });
-        }
-    }
-
-    play() {
-        this.videoElement.play();
-    }
-
-    pause() {
-        this.videoElement.pause();
-    }
-
-    stop() {
-        this.videoElement.pause();
-        this.videoElement.currentTime = 0;
-    }
-}
+    <script src="https://vjs.zencdn.net/7.11.4/video.min.js"></script>
+</body>
+</html>
